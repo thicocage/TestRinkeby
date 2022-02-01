2d0ceb26f81182bac5266be5bb3fa892273efc8f
thicocage <98658534+thicocage@users.noreply.github.com>
2022-02-01 22:58:19 +0300

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Gallery from "../components/Gallery";
+import FAQ from "../components/FAQ";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen w-full bg-primary">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
